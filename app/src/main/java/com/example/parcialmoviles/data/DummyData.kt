433f685ac3e74e26418e6543fb372a66f383acb9
Mauriaguilar7 <00195721@uca.edu.sv>
2023-05-22 13:43:20 -0600

    Add: capa modelo

---
+++ app/src/main/java/com/example/parcialmoviles/data/DummyData.kt
@@ -0,0 +1,11 @@
+package com.example.parcialmoviles.data
+
+import com.example.parcialmoviles.data.model.PeopleModel
+
+val name = "Mauricio"
+val lastname = "Aguilar"
+
+
+var people = mutableListOf(
+    PeopleModel(name,lastname)
+)
---
+++ app/src/main/java/com/example/parcialmoviles/data/model/PeopleModel.kt
@@ -0,0 +1,6 @@
+package com.example.parcialmoviles.data.model
+
+data class PeopleModel(
+    val name : String,
+    val lastName : String
+)
